cde8df5c9d576e8ee74cc0edbb42ded71827c3ed
Alessandro <sgrimush@gmail.com>
2025-11-22 14:29:23 -0300

    Update mudar_senha.py

--- page/mudar_senha.py
+++ page/mudar_senha.py
@@ -53,7 +53,8 @@ def update_user_password(engine, username, old_password, new_password):
 
 # --- Função Principal da Página ---
 
-def show_mudar_senha_page(engine, base_data_path):
+def show_mudar_senha_page(engine=None, base_data_path=None):
+
     """Cria a interface da página de alteração de senha."""
     
     st.title("🔑 Alterar Minha Senha")
