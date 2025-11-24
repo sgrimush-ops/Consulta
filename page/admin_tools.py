ce44d20e0603ab8a6f7a189718e6c40f63ba2336
Alessandro <sgrimush@gmail.com>
2025-11-24 08:39:08 -0300

    Update admin_tools.py

--- page/admin_tools.py
+++ page/admin_tools.py
@@ -8,11 +8,12 @@ def show_admin_tools(engine, base_data_path):
 
     # --- Upload do WMS ---
     st.subheader("Upload do WMS")
-    uploaded_wms = st.file_uploader("Selecione o arquivo WMS.xlsm", type="xlsm")
+    uploaded_wms = st.file_uploader("Selecione o WMS", type=["xlsm", "xlsx", "xls", "csv"], key="wms_uploader")
+    process_automatic_upload(uploaded_wms, wms_base, "wms")
     
     if uploaded_wms:
         # Define o caminho completo onde o arquivo será salvo
-        file_path = os.path.join(base_data_path, "WMS.xlsm")
+        file_path = os.path.join(base_data_path, "WMS.csv")
         try:
             # Salva o arquivo no disco persistente
             with open(file_path, "wb") as f:
@@ -23,10 +24,11 @@ def show_admin_tools(engine, base_data_path):
 
     # --- Upload do Histórico ---
     st.subheader("Upload do Histórico de Solicitações")
-    uploaded_hist = st.file_uploader("Selecione o arquivo historico_solic.xlsm", type="xlsm")
+    uploaded_hist = st.file_uploader("Selecione o Histórico", type=["xlsm", "xlsx", "xls", "csv"], key="hist_uploader")
+    process_automatic_upload(uploaded_hist, hist_base, "hist")
     
     if uploaded_hist:
-        file_path = os.path.join(base_data_path, "historico_solic.xlsm")
+        file_path = os.path.join(base_data_path, "historico_solic.csv")
         try:
             with open(file_path, "wb") as f:
                 f.write(uploaded_hist.getbuffer())
@@ -36,14 +38,16 @@ def show_admin_tools(engine, base_data_path):
 
     # --- Upload do Mix ---
     st.subheader("Upload do Mix Ativo")
-    uploaded_mix = st.file_uploader("Selecione o arquivo __MixAtivoSistema.xlsx", type="xlsx")
+    uploaded_mix = st.file_uploader("Selecione o Mix", type=["xlsx", "xls", "csv"], key="mix_uploader")
+    process_automatic_upload(uploaded_mix, mix_base, "mix")
     
     if uploaded_mix:
-        file_path = os.path.join(base_data_path, "__MixAtivoSistema.xlsx")
+        file_path = os.path.join(base_data_path, "__MixAtivoSistema.csv")
         try:
             with open(file_path, "wb") as f:
                 f.write(uploaded_mix.getbuffer())
             st.success("Arquivo __MixAtivoSistema.xlsx atualizado com sucesso!")
         except Exception as e:
             st.error(f"Falha ao salvar o arquivo: {e}")
+
 
